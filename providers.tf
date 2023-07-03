353036cd825ad5947a16b815ff301c869800f5b6
Ernest Acheampong <ernestacheampong@ernests-mbp.mynetworksettings.com>
2023-07-03 13:01:30 -0400

    lambda error

--- providers.tf
+++ providers.tf
@@ -5,10 +5,10 @@ terraform {
         source  = "hashicorp/aws"
         version = ">=3.75.1"
       }
-      cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "3.33.0"
-      }
+      # cloudflare = {
+      # source  = "cloudflare/cloudflare"
+      # version = "3.33.0"
+      # }
       archive = {
         source  = "hashicorp/archive"
         version = "~>2.0.0"
