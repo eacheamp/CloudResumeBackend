8911d67e75dc31662868201ea574f383504de142
Ernest Acheampong <ernestacheampong@ernests-mbp.mynetworksettings.com>
2023-07-03 12:54:40 -0400

    Testing circleci decode 21

--- variables.tf
+++ variables.tf
@@ -31,18 +31,14 @@ variable "api_gateway_region" {
   description = "The region in which to create/manage resources"
   default     = "us-east-1"
 }
-variable "api_gateway_account_id" {
-  type        = string
-  description = "The account ID in which to create/manage resources"
-}
-variable "lambda_function_name" {
-  type        = string
-  description = "The name of the Lambda function"
-}
-variable "lambda_function_arn" {
-  type        = string
-  description = "The ARN of the Lambda function"
-}
+# variable "lambda_function_name" {
+#   type        = string
+#   description = "The name of the Lambda function"
+# }
+# variable "lambda_function_arn" {
+#   type        = string
+#   description = "The ARN of the Lambda function"
+# }
 variable "http_api_stage_name" {
   type        = string
   description = "The name of the API Gateway stage"
