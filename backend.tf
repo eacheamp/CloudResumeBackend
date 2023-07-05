af072001325b3fcb6bf4521d13c0cf214a786b9d
Ernest Acheampong <ernestacheampong@ernests-mbp.mynetworksettings.com>
2023-07-05 18:50:47 -0400

    Added backend 3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "eacheampongterraform"
-    key     = "cloud_resume"
+    key     = "cloud_resume_backend.state"
     region  = "us-east-1"
     profile = "default"
   }
