82e0b2711ac0987746fa1fa503280d90cbdcee78
Ernest Acheampong <ernestacheampong@ernests-mbp.mynetworksettings.com>
2023-07-23 17:22:08 -0400

    added unittests

---
+++ tests/test_handler.py
@@ -0,0 +1,16 @@
+import unittest
+import VisitorCount
+
+class TestHandlerCase(unittest.TestCase):
+    def test_response(self):
+        print('Testing response of lambda...')
+        event       ={'siteStat_id': 'Count'}
+        result      = VisitorCount.handler(event, None)
+        print(result)
+        # Assertions
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertGreaterEqual(1, int(result['body'])) # Check if the site count is a positive integer
+
+if __name__=='__main__':
+    unittest.main()
